b3b64192bd3753b0b21d2522c7df0992f5cc9b8c
tristangms <nvisionfactory@gmail.com>
2024-10-15 04:23:04 +0200

    Add concurrent coroutines module and implement wait_n function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,7 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn wait_random n times with the specified max_delay."""
     delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
