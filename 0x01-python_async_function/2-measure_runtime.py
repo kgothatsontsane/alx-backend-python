17d435d6ea583f53e733e2adaf0965c00a8bcdd5
tristangms <nvisionfactory@gmail.com>
2024-10-15 04:53:46 +0200

    Refactor import statement in measure_runtime.py and add measure_time function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -4,8 +4,8 @@
 import asyncio
 import time
 
-
 wait_n = __import__('1-concurrent_coroutines').wait_n
+
 
 def measure_time(n: int, max_delay: int) -> float:
     """Measure the total execution time for wait_n(n, max_delay)
