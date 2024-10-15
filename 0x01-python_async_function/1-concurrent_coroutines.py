c1f7b30a15a58049587849c1e4f4f00a492dcf4e
tristangms <nvisionfactory@gmail.com>
2024-10-15 04:22:53 +0200

    Add concurrent coroutines module

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module for concurrent coroutines"""
+
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn wait_random n times with the specified max_delay."""
+    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    return sorted(delays)
